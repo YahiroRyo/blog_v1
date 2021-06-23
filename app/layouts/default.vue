4a7659edf49bca4f3bdd64464d84e0ec64531bb9
YahiroRyo <honakafn@gmail.com>
2021-06-23 15:00:09 +0900

    ナヴィゲーションを開いている時に、ヘッダーのナヴィゲーションを閉じるように設定

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,14 +1,18 @@
 <template>
   <v-app>
-    <TheHeader @click="header.isClickNavIcon = !header.isClickNavIcon" />
+    <TheHeader
+      @click="header.isClickNavIcon = !header.isClickNavIcon"
+      :is-open="header.isClickNavIcon"
+    />
     <TheHeaderNav
       :is-open-header-nav="header.isClickNavIcon && $store.state.windowState.scrollY != 0"
     />
     <v-main>
-      <v-container style="height: 100vh" class="d-flex" fluid>
+      <v-container style="min-height: 100vh" class="d-flex" fluid>
         <nuxt />
       </v-container>
     </v-main>
+    <TheFooter />
   </v-app>
 </template>
 
@@ -21,10 +25,12 @@
 <script lang="ts">
 import TheHeader from "~/components/TheHeader.vue";
 import TheHeaderNav from "~/components/TheHeaderNav.vue";
+import TheFooter from "~/components/TheFooter.vue";
 export default {
   components: {
     TheHeader,
     TheHeaderNav,
+    TheFooter,
   },
   data(): any {
     return {
