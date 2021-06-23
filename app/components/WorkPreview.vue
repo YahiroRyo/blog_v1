34e21e66b9db6bddbbd7e19b4654155ee206fa7c
YahiroRyo <honakafn@gmail.com>
2021-06-23 14:58:13 +0900

    ブログと作品に対応したタイトルとイメージをプレビューできるコンポーネントを作成

---
+++ app/components/WorkPreview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-row class="v-cursor-pointer" align-content="center" justify="center">
+      <v-card
+        width="500"
+        height="300"
+        @mouseover="isHover = true"
+        @mouseleave="isHover = false"
+        :style="'background-image: url(' + img + ')'"
+      >
+        <v-overlay absolute :value="isHover" class="text-h6">
+          {{ title }}
+        </v-overlay>
+      </v-card>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["img", "title"],
+  data(): any {
+    return {
+      isHover: false as boolean,
+    } as any;
+  },
+};
+</script>
