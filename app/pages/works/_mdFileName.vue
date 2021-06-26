ec7ba8d55a211ec59d7b7997aa39b48dbb50e942
YahiroRyo <honakafn@gmail.com>
2021-06-26 15:42:06 +0900

    とりあえずファイル作成

---
+++ app/pages/works/_mdFileName.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-html=""></div>
+</template>
+
+<script lang="ts">
+export default {
+  data(this: { $route: any }): any {
+    return {
+      md: null as any,
+    } as any;
+  },
+  created(): void {
+    // ここでmdファイルをfetch
+  },
+};
+</script>
