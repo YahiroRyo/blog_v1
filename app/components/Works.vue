5dd63e39ff8546feff2dae833ca33b3b11aa731b
YahiroRyo <honakafn@gmail.com>
2021-06-26 15:29:38 +0900

    データが取得できてない場合はロードを表示するようにした。

--- app/components/Works.vue
+++ app/components/Works.vue
@@ -1,9 +1,18 @@
 <template>
-  <v-row justify="center" align-content="center" class="mt-10">
-    <div v-for="n in loopNum" :key="n" class="mb-10 mx-5">
-      <WorkPreview :title="'テスト' + n" :img="n" />
-    </div>
-  </v-row>
+  <div class="mt-10">
+    <v-row v-if="loopNum" justify="center" align-content="center">
+      <div v-for="n in loopNum" :key="n" class="mb-10 mx-5">
+        <WorkPreview :title="'テスト' + n" :img="n" :to="'/works/' + n" />
+      </div>
+    </v-row>
+    <v-row v-else justify="center" align-content="center" style="height: 300px">
+      <v-progress-circular
+        size="64"
+        indeterminate
+        color="gray darken-1"
+      ></v-progress-circular>
+    </v-row>
+  </div>
 </template>
 <script lang="ts">
 import WorkPreview from "~/components/WorkPreview.vue";
