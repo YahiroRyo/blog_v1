3bb530cdde96abce755364a5233f63c3ff879730
YahiroRyo <honakafn@gmail.com>
2021-06-26 15:41:24 +0900

    ブログ一覧と作品一覧というタイトルを追加

--- app/pages/blogs/index.vue
+++ app/pages/blogs/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
+  <div class="mx-auto">
+    <Title title="ブログ一覧" />
     <Blogs num="10" />
   </div>
 </template>
 <script lang="ts">
 import Blogs from "~/components/Blogs.vue";
+import Title from "~/components/Title.vue";
 
 export default {
   components: {
     Blogs,
+    Title,
   },
   data(): void {
     return {} as any;
--- app/pages/works/index.vue
+++ app/pages/works/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
+  <div class="mx-auto">
+    <Title title="作品一覧" />
     <Works num="10" />
   </div>
 </template>
 <script lang="ts">
 import Works from "~/components/Works.vue";
+import Title from "~/components/Title.vue";
 
 export default {
   components: {
     Works,
+    Title,
   },
   data(): void {
     return {} as any;
