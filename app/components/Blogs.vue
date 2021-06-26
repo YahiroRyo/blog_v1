ad5a25c4afb4718ab9093c2d378d9f394abb1592
YahiroRyo <honakafn@gmail.com>
2021-06-26 15:32:41 +0900

    データが取得できてない場合はロードを表示するようにした。 データ取得処理を追加

--- app/components/Blogs.vue
+++ app/components/Blogs.vue
@@ -1,12 +1,22 @@
 <template>
-  <v-row justify="center" align-content="center" class="mt-10">
-    <div v-for="n in loopNum" :key="n" class="mb-10 mx-5">
-      <WorkPreview :title="'テスト' + n" :img="n" />
-    </div>
-  </v-row>
+  <div class="mt-10">
+    <v-row cols="15" v-if="result.length != 0" justify="center" align-content="center">
+      <v-col v-for="(item, index) in result" :key="index" class="mb-10 mx-5">
+        <WorkPreview :title="item.title" :img="item.img" :to="`/blogs/${item.fileId}`" />
+      </v-col>
+    </v-row>
+    <v-row v-else justify="center" align-content="center" style="height: 300px">
+      <v-progress-circular
+        size="64"
+        indeterminate
+        color="gray darken-1"
+      ></v-progress-circular>
+    </v-row>
+  </div>
 </template>
 <script lang="ts">
 import WorkPreview from "~/components/WorkPreview.vue";
+import { $axios } from "~/utils/api";
 
 export default {
   props: { num: { type: String } },
@@ -15,12 +25,31 @@ export default {
   },
   data(props: { num: String }): any {
     return {
-      loopNum: Number(props.num) as Number,
+      fetchNum: Number(props.num) as Number,
+      result: [] as Array<any>,
     } as any;
   },
-  created(): void {
+  async created(this: {
+    result: Array<any>;
+    fetchNum: Number;
+    $config: any;
+  }): Promise<void> {
     // Blogをnum分fetch
     // numが-1だったら50個ずつfetch
+    const param = {
+      params: {
+        num: this.fetchNum as Number,
+        sum: 0 as Number,
+      } as any,
+    } as any;
+    if (this.fetchNum == -1) {
+      // 50個fetch
+      param.params.num = 50;
+      this.result = await $axios.$get(`/blogs/get`, param);
+    } else {
+      // fetchNum分fetch
+      this.result = await $axios.$get(`/blogs/get`, param);
+    }
   },
 };
 </script>
