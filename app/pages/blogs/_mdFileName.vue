6f6b8a2dde49739be7432ae7ab9db2687031bd85
YahiroRyo <honakafn@gmail.com>
2021-06-26 15:41:55 +0900

    Blogのデータを取得する処理を追加

--- app/pages/blogs/_mdFileName.vue
+++ app/pages/blogs/_mdFileName.vue
@@ -1,16 +1,45 @@
 <template>
-  <div v-html="md.default"></div>
+  <div>
+    <div v-if="md" v-html="md"></div>
+    <v-overlay :value="true" v-else>
+      <v-row cols="15" justify="center" align-content="center">
+        <v-col class="text-center">
+          <v-progress-circular indeterminate size="96"></v-progress-circular>
+          <p class="display-1 mt-10">サーバを起動中か、データを取得中です。</p>
+          <p class="body-1">
+            ※30秒立っても見れない場合は、ページを抜けていただいて構いません
+          </p>
+        </v-col>
+      </v-row>
+    </v-overlay>
+  </div>
 </template>
 
 <script lang="ts">
+import { $axios } from "~/utils/api";
+import { Route } from "vue-router";
+
 export default {
   data(this: { $route: any }): any {
     return {
-      md: null as any,
+      result: null as any,
+      md: "" as string,
     } as any;
   },
-  created(): void {
+  async created(this: {
+    result: any;
+    md: string;
+    $md: { render: Function };
+    $route: Route;
+  }): Promise<void> {
     // ここでmdファイルをfetch
+    const param = {
+      params: {
+        fileId: this.$route.params.mdFileName,
+      },
+    };
+    this.result = await $axios.$get("/blog/get", param);
+    this.md = this.$md.render(this.result.md);
   },
 };
 </script>
