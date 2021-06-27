fba670f296b4d4ca8c29b0251d49af302bc967b4
YahiroRyo <honakafn@gmail.com>
2021-06-28 00:12:11 +0900

    work関係のルーティングを追加

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\WorkController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,13 @@ use App\Http\Controllers\BlogController;
 Route::get('/blogs/get', [BlogController::class, 'index']);
 Route::get('/blog/get', [BlogController::class, 'show']);
 Route::post('/blog/create', [BlogController::class, 'create']);
+
+Route::get('/works/get', [WorkController::class, 'index']);
+Route::get('/work/get', [WorkController::class, 'show']);
+Route::post('/work/create', [WorkController::class, 'create']);
+
+Route::get('/is-auth', [AuthController::class, 'is_auth']);
+Route::post('/auth', [AuthController::class, 'auth']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
