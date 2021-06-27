70e0b9e096ad7776ea153360c031a7ae5f005112
YahiroRyo <honakafn@gmail.com>
2021-06-28 00:08:45 +0900

    認証関係のコントローラを作成 認証しているかを確認する処理を作成 認証する処理を作成

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class AuthController extends Controller
+{
+    public function reset_auth(Request $request): void {
+        $request->session()->put('is_auth', false);
+    }
+    public function is_auth(Request $request): array {
+        if ($request->session()->get('is_auth') !== null) {
+            return ['is_auth' => $request->session()->get('is_auth')];
+        } else {
+            $request->session()->put('is_auth', false);
+            return ['is_auth' => false];
+        }
+    }
+    public function auth(Request $request): array {
+        // $request->authPass 認証のパスワード
+        $auth_pass = $request->authPass;
+        if (config('app.auth_password') == $auth_pass) {
+            $request->session()->put('is_auth', true);
+            return ['is_auth' => true];
+        }
+        return ['is_auth' => false];
+    }
+}
