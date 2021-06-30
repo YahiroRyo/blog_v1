7bc15f3e5a25404109b6665dd8c0acbb3d554d6d
YahiroRyo <honakafn@gmail.com>
2021-06-30 19:28:49 +0900

    404を作成

---
+++ app/pages/errors/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row style="height: 80vh" justify="center" align-content="center">
+    <div class="text-h2">ページが見つかりません</div>
+  </v-row>
+</template>
+<script lang="ts">
+import { Store } from "vuex";
+
+export default {
+  created(this: { $store: Store<any> }): void {
+    this.$store.commit("windowState/setTitle", "ページが見つかりません");
+    this.$store.commit(
+      "windowState/setDescription",
+      "YAPPIブログ | 404 エンジニアに役立つ情報をお届け(?) Laravel vue nuxt html css scss javascriptらへんやってます"
+    );
+  },
+};
+</script>
