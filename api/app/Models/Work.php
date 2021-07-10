6a5ceb470cad9d0d5529f965019305a624f5a523
YahiroRyo <honakafn@gmail.com>
2021-07-10 09:54:47 +0900

    WorkとWorkTagを繋げた

--- api/app/Models/Work.php
+++ api/app/Models/Work.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use App\Models\WorkTag;
 
 class Work extends Model
 {
@@ -15,4 +16,7 @@ class Work extends Model
         'title',
         'file_id',
     ];
+    public function work_tag() {
+        return $this->hasMany(WorkTag::class);
+    }
 }
