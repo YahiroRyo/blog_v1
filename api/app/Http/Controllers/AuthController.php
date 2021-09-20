f802c10d07c3ed1994c9263148ec67eb46aeb779
YahiroRyo <honakafn@gmail.com>
2021-09-20 14:50:40 +0900

    認証方法を変更

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -9,43 +9,21 @@ use App\Models\Admin;
 class AuthController extends Controller
 {
     public function reset_auth(Request $request): void {
-        $is_remember_token_query = Admin::where('remember_token', $request->cookie('laravel_session'));
-        if ($is_remember_token_query->exists()) {
-            $admin = $is_remember_token_query->first();
-            $admin->fill([
-                'is_auth' => false,
-            ]);
-            $admin->save();
-        }
+        $request->session()->put('is_auth', false);
     }
     public function is_auth(Request $request): array {
-        $is_remember_token_query = Admin::where('remember_token', $request->cookie('laravel_session'));
-        if ($is_remember_token_query->exists()) {
-            if ($is_remember_token_query->first()['is_auth']) {
-                return ['is_auth' => true];
-            }
+        if ($request->session()->get('is_auth') !== null) {
+            return ['is_auth' => $request->session()->get('is_auth')];
+        } else {
+            $request->session()->put('is_auth', false);
+            return ['is_auth' => false];
         }
-        return ['is_auth' => false];
     }
     public function auth(Request $request): array {
         // $request->authPass 認証のパスワード
         $auth_pass = $request->authPass;
-        $is_remember_token_query = Admin::where('remember_token', $request->cookie('laravel_session'));
         if (config('app.auth_password') == $auth_pass) {
-            if ($is_remember_token_query->exists()) {
-                $admin = $is_remember_token_query->first();
-                $admin->fill([
-                    'is_auth' => true,
-                ]);
-                $admin->save();
-            } else {
-                $admin = new Admin();
-                $admin->fill([
-                    'is_auth' => true,
-                    'remember_token' => $request->cookie('laravel_session'),
-                ]);
-                $admin->save();
-            }
+            $request->session()->put('is_auth', true);
             return ['is_auth' => true];
         }
         return ['is_auth' => false];
