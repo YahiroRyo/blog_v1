673259cadebe2c0d65617596b5271f6f79521025
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 02:33:53 +0900

    source追加

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,7 @@ FILE="auto_start.sh"
 cd /home/ec2-user
 
 if [ -e $FILE ]; then
+  source /home/ec2-user/.bash_profile
   export NVM_DIR="~/.nvm"
   su -l ec2-user -c "sudo sh ~/.nvm/nvm.sh" 
   npm install
