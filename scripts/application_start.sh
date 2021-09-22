798ced83ed5d15524dbf4040fed6f0a2858771ee
YahiroRyo <honakafn@gmail.com>
2021-09-22 19:06:16 +0900

    npmコマンドが使えなかったため使えるように修正

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,9 @@ FILE="auto_start.sh"
 cd /home/ec2-user
 
 if [ -e $FILE ]; then
+  if [[ -s ~/.nvm/nvm.sh ]] ; then
+    source ~/.nvm/nvm.sh ;
+  fi
   npm install
   su -l ec2-user -c "sh ~/auto_start.sh"
 else
