7d1b0eed887ff17447e13d28801c97edf6532596
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 02:29:36 +0900

    パスを変更

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,7 +5,7 @@ cd /home/ec2-user
 
 if [ -e $FILE ]; then
   export NVM_DIR="$HOME/.nvm"
-  su -l ec2-user -c "sudo sh $NVM_DIR/nvm.sh" 
+  su -l ec2-user -c "sudo sh ~/.nvm/nvm.sh" 
   su -l ec2-user -c "sudo sh ~/auto_start.sh"
 else
   cd /var/www
