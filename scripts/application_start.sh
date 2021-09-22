0ca681563f9bac520e94f9bcb5d57e3dc091a72e
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 02:30:46 +0900

    npm installを追加

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,6 +6,7 @@ cd /home/ec2-user
 if [ -e $FILE ]; then
   export NVM_DIR="$HOME/.nvm"
   su -l ec2-user -c "sudo sh ~/.nvm/nvm.sh" 
+  npm install
   su -l ec2-user -c "sudo sh ~/auto_start.sh"
 else
   cd /var/www
