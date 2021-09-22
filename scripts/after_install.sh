ce2e27e053fe8dcdddb8f4de3af5d4c70a081134
YahiroRyo <honakafn@gmail.com>
2021-09-22 19:12:46 +0900

    npmコマンドをexportする処理をafter_installに移動

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,6 +5,9 @@ cd /home/ec2-user
 
 if [ -e $FILE ]; then
   # appの場合
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   sudo rm -rf /home/ec2-user/*
   sudo mv -f /var/tmp/app/* /home/ec2-user/
 else
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,9 +4,6 @@ FILE="auto_start.sh"
 cd /home/ec2-user
 
 if [ -e $FILE ]; then
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   npm install
   su -l ec2-user -c "sh ~/auto_start.sh"
 else
