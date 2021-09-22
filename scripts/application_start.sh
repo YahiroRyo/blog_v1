42843468d468bba12f890bd41a421db7b0aeb937
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 02:09:36 +0900

    .bash_profileをsource

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 
 FILE="auto_start.sh"
 cd /home/ec2-user
 
 if [ -e $FILE ]; then
+  
   npm install
   su -l ec2-user -c "sh ~/auto_start.sh"
 else
