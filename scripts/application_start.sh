4e73b4bc89b0cd035a84706d83d06b6bfca613ab
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 02:19:14 +0900

    nodeとnpmをインストールしてみる

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,7 +4,11 @@ FILE="auto_start.sh"
 cd /home/ec2-user
 
 if [ -e $FILE ]; then
-  export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  apt update
+  apt install nodejs -y
+  apt install npm -y
+  npm i npm@latest -g
+  npm i n -g
   npm install
   su -l ec2-user -c "sh ~/auto_start.sh"
 else
