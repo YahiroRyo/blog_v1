ede11acfe850d51d8a20f8ff18caebb3ffcb362b
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-22 18:32:04 +0900

    sudoを追加・ファイルの拡張子に.shを追加

---
+++ scripts/after_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILE="auto_start.sh"
+
+if [ -e $FILE ]; then
+  # appの場合
+  sudo mv -f /home/ec2-user/tmp/app/* /home/ec2-user/
+else
+  # apiの場合
+  sudo mv -f /home/ec2-user/tmp/api/* /var/www/
+fi
