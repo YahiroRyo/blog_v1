8dfb37b697927089fa2a740b9e7bb9fbf3b88878
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 02:17:49 +0900

    スクリプト追加

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
-source /home/ec2-user/.bash_profile
+#!/bin/bash
 
 FILE="auto_start.sh"
 cd /home/ec2-user
 
 if [ -e $FILE ]; then
-  
+  export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   npm install
   su -l ec2-user -c "sh ~/auto_start.sh"
 else
