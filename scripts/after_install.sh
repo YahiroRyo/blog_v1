5d8459822f6273a486325dac36f138e6da7c7d28
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 02:03:03 +0900

    sourceでnpmを使えるようにした

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,9 +5,7 @@ cd /home/ec2-user
 
 if [ -e $FILE ]; then
   # appの場合
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  source ~/.nvm/nvm.sh
   sudo rm -rf /home/ec2-user/*
   sudo mv -f /var/tmp/app/* /home/ec2-user/
 else
