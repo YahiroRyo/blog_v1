8e856e92d8c08a6318b28c95f0539961fd0ada96
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-22 18:40:39 +0900

    /home/ec2-userに移動する処理を追加・ディレクトリの中身を削除する処理を追加

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 FILE="auto_start.sh"
+cd /home/ec2-user
 
 if [ -e $FILE ]; then
   # appの場合
-  sudo mv -f /home/ec2-user/tmp/app/* /home/ec2-user/
+  sudo rm -rf /home/ec2-user/*
+  sudo mv -f /var/tmp/app/* /home/ec2-user/
 else
   # apiの場合
-  sudo mv -f /home/ec2-user/tmp/api/* /var/www/
+  sudo rm -rf /var/www/*
+  sudo mv -f /var/tmp/api/* /var/www/
 fi
