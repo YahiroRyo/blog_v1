0275098d2ae7398d44bbb60d5ec7aee88827f6c8
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-22 11:39:19 +0900

    エラー吐いたので原因調査

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,9 +5,7 @@ FILE = "auto_start.sh"
 if [ -e $FILE ]; then
   # appの場合
   sudo mv /home/ec2-user/tmp/app/* /home/ec2-user/
-  sudo mv /home/ec2-user/tmp/app/.* /home/ec2-user/
 else
   # apiの場合
   sudo mv /home/ec2-user/tmp/api/* /var/www
-  sudo mv /home/ec2-user/tmp/api/.* /var/www
 fi
