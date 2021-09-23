dd48e2b46a2117fa755aa943df3b176b98207778
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 13:39:50 +0900

    npm startに変更

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,7 +5,7 @@ cd /home/ec2-user
 
 if [ -e $FILE ]; then
   node --max-old-space-size=256 $(which npm) install
-  su -l ec2-user -c "sh ~/auto_start.sh"
+  nohup npm start
 else
   cd /var/www
   composer update
