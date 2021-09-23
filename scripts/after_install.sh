7ae41ee6901c9f99e8224dee624c3b2b48cbf67f
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 18:52:33 +0900

    applicatoin_start.shの内容を一部コピー

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,9 +5,12 @@ cd /home/ec2-user
 
 if [ -e $FILE ]; then
   # appの場合
-  source ~/.nvm/nvm.sh
   sudo rm -rf /home/ec2-user/*
   sudo mv -f /var/tmp/app/* /home/ec2-user/
+  dd if=/dev/zero of=/swapfile bs=1M count=1024
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
 else
   # apiの場合
   sudo rm -rf /var/www/*
