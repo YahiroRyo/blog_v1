89bbe46a8aa465de10383786a50c3e00d9fca4b1
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 18:29:24 +0900

    suが使えるか検証

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,6 +5,12 @@ cd /home/ec2-user
 
 if [ -e $FILE ]; then
   source /home/ec2-user/.bash_profile
+  sudo su
+  dd if=/dev/zero of=/swapfile bs=1M count=1024
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
+  exit
   npm install
 else
   cd /var/www
