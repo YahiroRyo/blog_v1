f852d5adfcc0ab79405b0d4870a3316d0498c361
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-23 13:46:29 +0900

    nohup node startに変更

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,7 +5,7 @@ cd /home/ec2-user
 
 if [ -e $FILE ]; then
   node --max-old-space-size=256 $(which npm) install
-  nohup npm start
+  nohup node --max-old-space-size=256 $(which npm) start
 else
   cd /var/www
   composer update
