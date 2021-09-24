f6aaa8112bca47f420c9832ccc1a93f6e7527c90
YahiroRyo <YahiroRyo@users.noreply.github.com>
2021-09-24 11:59:20 +0000

    Apply fixes from StyleCI

--- api/app/Http/Controllers/BlogController.php
+++ api/app/Http/Controllers/BlogController.php
@@ -121,8 +121,7 @@ class BlogController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -132,8 +131,7 @@ class BlogController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param int $id
-     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show(Request $request)
@@ -173,8 +171,7 @@ class BlogController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param int $id
-     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function edit(Request $request)
@@ -231,9 +228,8 @@ class BlogController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param int                      $id
-     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request)
@@ -244,8 +240,7 @@ class BlogController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param int $id
-     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function delete(Request $request): void
--- api/app/Http/Middleware/ForceHttps.php
+++ api/app/Http/Middleware/ForceHttps.php
@@ -10,9 +10,8 @@ class ForceHttps
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure                 $next
-     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
