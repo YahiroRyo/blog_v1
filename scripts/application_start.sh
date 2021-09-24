c90132782d72bdcffd8fd524be5555b4f5bdda3d
YahiroRyo <76673979+YahiroRyo@users.noreply.github.com>
2021-09-24 22:01:39 +0900

    Update application_start.sh
    npm startのプロセスをkillして、非同期で実行

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,6 +6,8 @@ cd /home/ec2-user
 if [ -e $FILE ]; then
   source /home/ec2-user/.bash_profile
   npm install
+  ps aux | grep "npm start" | awk '{ print "kill -9", $2}' | sh
+  nohup npm start
 else
   cd /var/www
   composer update
